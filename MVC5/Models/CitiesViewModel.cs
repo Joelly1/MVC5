919b684093ec048e69aea70d1d4de1a74484c45c
joel <olugaabiola18@gmail.com>
2024-01-07 21:37:36 +0100

    Add project files.

---
+++ MVC5/Models/CitiesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5.Models
+{
+	public class CitiesViewModel
+	{
+		public IEnumerable<SelectListItem> Cities { set; get; }
+		public IEnumerable<string> SelectedCities { set; get; }
+	}
+}
---
+++ MVC5/Models/Company.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5.Models
+{
+	public class Company
+	{
+		public string SelectDepartment { get; set; }
+		public List<Department> departments
+		{
+			get
+			{
+				SampleDB db = new SampleDB();
+				return db.Departments.ToList();
+			}
+		}
+		
+	}
+}
